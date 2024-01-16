543a62eec897c87bde96ef5330dc075f3c07cb6d
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-16 13:56:09 +0100

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n,
+write a method that calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All
+=> Paste => HHHHHH => Paste => HHHHHHHHH
+
+Number of operations: 6
+"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
